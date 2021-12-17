af63d9277e9e8d64bbbc25ef63da8f895dffe35a
kouhei-sugahara <200092tw@yse-c.net>
2021-12-17 14:24:59 +0900

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,28 @@
+//HTMLがロードされたら実行
+$(() => {
+    const url  = ''
+    let socket = io.connect(url)
+
+    const message = $('#message')
+    const chatList = $('#chatList')
+
+    $('#send').on('click',() => {
+        //メッセージ入力チェック
+        if(!message.val()) return
+        //サーバにデータ送信
+        socket.emit('message',
+            {
+                message: message.val()
+            }
+        )
+        //メッセージを空にする
+        message.val('')
+    })
+    //データ受信
+    socket.on('message',(data) =>{
+        console.log(data)
+        let chatElement = $('<p>').append(data.message)
+        //console.log(chatElement)
+        chatList.prepend(chatElement)
+    })
+})
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended: true}))
+
+const dotenv = require('dotenv')
+dotenv.config()
+const host = process.env.HOST
+const port = process.env.PORT
+
+io.on('connection',(socket) => {
+    //クライアントからデータ受信
+    socket.on('message',(data) => {
+        console.log(data)
+        //すべてのクライアントにデータ送信
+        io.emit('message',data)
+    })
+})
+
+http.listen(port,host, () => {
+    console.log('http://' + host + ':' + port)
+})
